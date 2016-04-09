dac127f4b6aedf6258e8561d2a4075d492a9368e
Keijack <keijack.wu@gmail.com>
2016-04-09 11:12:17 +0800

    add some comments.

--- src/test/java/com/keijack/orm/sqlfile/entity/test.sql
+++ src/test/java/com/keijack/orm/sqlfile/entity/test.sql
@@ -1,6 +1,8 @@
+-- some comments here....
 --#count(ID)
 select ID, name, CREATETIME as CT from TEST
 	where 1 = 1
+-- some comments here ...
 	and ID = :idEquals	and NAME like :nameLike@object[normal]
 	and ID in (:idIn@list) 
 	order by ID :idOrder@fragment[optional] -- :orderbyid@fragment
